e48cb7b1a4964fc2b417bb684493bc159fea5d2e
RD <romualdas@prodev.lt>
2020-09-14 13:57:52 +0300

    wip

--- wp-content/themes/litas/templates/indeliai.php
+++ wp-content/themes/litas/templates/indeliai.php
@@ -9,7 +9,7 @@ get_header();
 
 <div class="main-content-wrapper flex-fill position-relative">
     <div class="main-content animate-links">
-        <div class="main-padding-top">
+        <div class="main-padding-top section">
             <div class="container">
                 <h1 class="section-title section-title-max-w">Didelės terminuotų indėlių palūkanos</h1>
 
@@ -55,6 +55,26 @@ get_header();
                     </tr>
                     </tbody>
                 </table>
+
+                <ul class="centered-list">
+                    <li>Minimali indėlio suma 1000 eurų</li>
+                    <li>Indėlio papildymas bet kuriuo metu (min. suma 1000 eurų)</li>
+                    <li>Indėlio palūkanos išmokamos kas 3 mėnesius</li>
+                    <li>Automatinis indėlio pratęstimas</li>
+                </ul>
+            </div>
+            <div class="section-cta">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-8">
+                            <h2 class="section-cta-title">Litas kredito unijos indėliai iki 100 000 eurų yra apdrausti valstybinėje įmonėje “Indėlių ir investicijų draudimas”</h2>
+                            <a href="#" class="section-cta-link">Papildoma informacija indėlininkams</a>
+                        </div>
+                        <div class="col text-center text-lg-right">
+                            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/IID-Logotipas 1.png" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
