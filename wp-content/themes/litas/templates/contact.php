869b22063452b6576c52624c5ff3dc1044b813fc
RD <romualdas@prodev.lt>
2020-09-15 13:35:23 +0300

    wip

--- wp-content/themes/litas/templates/contact.php
+++ wp-content/themes/litas/templates/contact.php
@@ -1,33 +1,58 @@
 <?php
 /*
 Template Name: Contact
-Template Post Type: page
 */
 
 get_header();
 ?>
 
-<div class="main-padding-top">
-    <div class="section-contact">
-        <?php
-        if ( have_posts() ) :
-            while ( have_posts() ) : the_post();
-                ?>
-                <div class="container">
-                    <h1 class="section-title text-center mb-7"><?php the_title(); ?></h1>
-                    <div class="row justify-content-center">
-                        <div class="col-lg-10 col-xl-8">
-                            <div class="section-subtitle lead lh-xl text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+<div class="main-content-wrapper flex-fill position-relative">
+    <div class="main-content animate-links">
+        <div class="main-padding-top section">
+            <div class="container">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <h1 class="section-title section-title-max-w"><?php the_title(); ?></h1>
+                    <div class="row section-contacts">
+                        <div class="col-md-6 section-contacts-left">
+                            <div>Taikos pr. 21B, LT-50210 Kaunas</div>
+                            <div>+370 37 330 779 <span class="separator"></span> +370 685 62121</div>
+                            <div><a href="mailto:labas@unijalitas.lt">labas@unijalitas.lt</a></div>
+                        </div>
+                        <div class="col-md-6 section-contacts-right">
+                            <div class="section-contacts-right-title">Kredito unija LITAS</div>
+                            <div>Įmonės kodas: 302448934</div>
+                            <div>Atsiskaitomoji sąskaita: LT00 0000 0000 0000</div>
+                            <div>Banko kodas: AA0000</div>
                         </div>
                     </div>
-                </div>
-                <div class="embed-responsive embed-responsive-12by5 pt-special-mobile bg-light">
-                    <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2293.504933506549!2d23.941476851999766!3d54.911611063920695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e7188e4219debb%3A0x3a5b5263186bb0e3!2sTaikos%20pr.%2021B%2C%20Kaunas%2050210!5e0!3m2!1slt!2slt!4v1599740747390!5m2!1slt!2slt" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                </div>
-                <?php
-            endwhile;
-        endif;
-        ?>
+                    <div class="section-working-hours">
+                        <div class="working-hours-left">
+                            <h4 class="working-hours-title">Darbo laikas</h4>
+                            <div class="working-hours-h">
+                                <div class="working-hours-row">
+                                    <div>I-IV</div>
+                                    <div>8:30 – 17:30</div>
+                                </div>
+                                <div class="working-hours-row">
+                                    <div>V</div>
+                                    <div>8:30 – 16:30</div>
+                                </div>
+                                <div class="working-hours-row">
+                                    <div>VI-VII</div>
+                                    <div>Nedirbame</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="working-hours-right">
+                            <img class="working-hours-image" src="<?php echo get_stylesheet_directory_uri()?>/images/office.jpg" alt="">
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+            <div>
+                <?php echo do_shortcode('[wpgmza id="1"]') ?>
+            </div>
+        </div>
     </div>
 </div>
 
