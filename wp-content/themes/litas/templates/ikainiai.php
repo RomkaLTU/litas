14823ec41901040b60d1f09b16c8c95c8d81a57d
RD <romualdas@prodev.lt>
2020-09-16 15:07:48 +0300

    wip

--- wp-content/themes/litas/templates/ikainiai.php
+++ wp-content/themes/litas/templates/ikainiai.php
@@ -5,6 +5,8 @@ Template Name: Įkainiai
 */
 
 get_header();
+
+$items = get_field('items');
 ?>
 
 <div class="main-content-wrapper flex-fill position-relative">
@@ -21,138 +23,29 @@ get_header();
                         </tr>
                         </thead>
                         <tbody>
-                        <tr class="first">
-                            <td>1. Vietiniai kredito pervedimai eurais</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Kredito unijos viduje į savo sąskaitą (kredito unijoje)</td>
-                            <td>0,00 EUR</td>
-                        </tr>
-                        <tr>
-                            <td>Kredito unijos viduje į savo sąskaitą (internetu)</td>
-                            <td>0,00 EUR</td>
-                        </tr>
-                        <tr>
-                            <td>Kredito unijos viduje į kito kliento sąskaitą (kredito unijoje)</td>
-                            <td>0,29 EUR</td>
-                        </tr>
-                        <tr>
-                            <td>Kredito unijos viduje į kito kliento sąskaitą (internetu)</td>
-                            <td>0,14 EUR</td>
-                        </tr>
-                        <tr>
-                            <td>Į kitas Lietuvoje veikiančias kredito įstaigas (kredito unijoje)</td>
-                            <td>0,58 EUR</td>
-                        </tr>
-                        <tr>
-                            <td>Į kitas Lietuvoje veikiančias kredito įstaigas (internetu)</td>
-                            <td>0,58 EUR</td>
-                        </tr>
-                        <tr class="last">
-                            <td>Kredito pervedimo atšaukimas (jei pervedimas nėra įvykdytas)</td>
-                            <td>0,00 EUR</td>
-                        </tr>
-                        <tr class="first">
-                            <td>2. Gautų lėšų įskaitymas (administravimas)</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Į kitas Lietuvoje veikiančias kredito įstaigas (internetu)</td>
-                            <td>0,09 EUR</td>
-                        </tr>
-                        <tr>
-                            <td>Lėšos, gautos iš kitų Lietuvoje veikiančių kredito įstaigų</td>
-                            <td>0,09 EUR</td>
-                        </tr>
-                        <tr class="last">
-                            <td>Lėšos, gautos kredito unijos viduje</td>
-                            <td>2,32 EUR</td>
-                        </tr>
-                        <tr class="first">
-                            <td>3. Sąskaitos tvarkymas</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Sąskaitos tvarkymas</td>
-                            <td>0,00 EUR</td>
-                        </tr>
-                        <tr>
-                            <td>Sąskaitos uždarymą</td>
-                            <td>0,00 EUR</td>
-                        </tr>
-                        <tr class="last">
-                            <td>Sąskaitos išrašo parengimas (kredito unijoje)</td>
-                            <td>0,58 EUR</td>
-                        </tr>
-                        <tr class="first">
-                            <td>4. Grynųjų pinigų operacijso eurais</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div class="underline mb-3">4.1. Grynųjų pinigų išmokėjimas</div>
-                            </td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div class="pb-3">Iš sąskaitos kredito unijoje</div>
-                            </td>
-                            <td>
-                                <div class="max-w-220px mx-auto">0,00 EUR iki 144,81 EUR/mėn. (viršijus 0,4 proc.)</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div class="underline mb-3">4.2. Grynųjų pinigų įmokėjimas</div>
-                            </td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div>Į savo sąskaitą (kredito unijoje)</div>
-                            </td>
-                            <td>
-                                0,00 EUR
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div>Į kito asmens sąskaitą</div>
-                            </td>
-                            <td>
-                                0,00 EUR
-                            </td>
-                        </tr>
-                        <tr class="last">
-                            <td>Į kito asmens sąskaitą</td>
-                            <td>0,00 EUR</td>
-                        </tr>
-                        <tr class="first">
-                            <td>5. Įmokos už paslaugas</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Grynaisiais pinigais</td>
-                            <td>0,58 EUR</td>
-                        </tr>
-                        <tr class="last">
-                            <td>Internetu</td>
-                            <td>0,14 EUR</td>
-                        </tr>
-                        <tr class="first">
-                            <td>Papildoma infomacija:</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Stojamasis mokestis</td>
-                            <td>15,00 EUR</td>
-                        </tr>
-                        <tr class="last">
-                            <td>Pajinis įnašas</td>
-                            <td>30,00 EUR</td>
-                        </tr>
+                        <?php foreach ($items as $item): ?>
+                            <?php if ($item['bold']): ?>
+                                <tr class="first">
+                            <?php elseif ($item['last']): ?>
+                                <tr class="last">
+                            <?php else: ?>
+                                <tr>
+                            <?php endif; ?>
+                                <td>
+                                    <?php if ($item['underline']): ?>
+                                        <div class="underline mb-3"><?php echo $item['title'] ?></div>
+                                    <?php else: ?>
+                                        <?php echo $item['title'] ?>
+                                    <?php endif; ?>
+                                </td>
+                                <td>
+                                    <div class="max-w-220px mx-auto">
+                                        <?php echo $item['price'] ?>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
                     </table>
                 </div>
             </div>
